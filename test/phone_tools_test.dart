817bead1677706e6a67400b548ecd43e5af8b3f0
Vitor Melo <vitor.melo@cmsw.com>
2024-04-22 18:51:57 -0300

    feat: Add function to get a list of calling codes

--- test/phone_tools_test.dart
+++ test/phone_tools_test.dart
@@ -1,1 +1,12 @@
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+import 'package:phone_tools/phone_tools.dart';
+
+void main() {
+  test('get USA calling code', () {
+    expect(PhoneTools.callingCodes()["United States"], "+1");
+  });
+
+  test('get Error calling code', () {
+    expect(PhoneTools.callingCodes()["Error"], null);
+  });
+}
